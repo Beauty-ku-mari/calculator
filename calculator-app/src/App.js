c5504e6fc25d5cd69ebe7e3b057fdc101ebf1d49
Beauty kumari <kbeauty193@gmail.com>
2024-10-01 23:37:21 +0530

    commit

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,4 +1,15 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
 
 
